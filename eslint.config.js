c8f78d402f6732e7962279126ab3155fd55e68ee
Alimardon <alisobirzanov29@gmail.com>
2024-01-14 21:48:12 +0500

    init 0.0.1

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  vue: true,
+  typescript: true,
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+    semi: false,
+  },
+  rules: {
+    'antfu/top-level-function': 'off',
+    'prefer-exponentiation-operator': 'off',
+    'unicorn/prefer-number-properties': 'off',
+    'ts/no-use-before-define': 'off',
+    'prefer-template': 'off',
+    'curly': 'off',
+    'no-console': 'warn',
+  },
+});
